f2b40472c2e6c5ad5e0ed42c0a29f4e2405b65e1
AliceM80 <a.mutke@hotmail.de>
2022-04-21 13:04:11 +0200

    modify html.files

--- src/main/java/com/finalproject/model/repository/UserRepository.java
+++ src/main/java/com/finalproject/model/repository/UserRepository.java
@@ -33,11 +33,18 @@ public interface UserRepository extends JpaRepository<User, Long> {
     int enableUser(String email);
 
 
-    @Query(value="SELECT e FROM User e WHERE e.firstName LIKE %:keyword% " +
-            "OR e.lastName LIKE %:keyword% OR e.username LIKE %:keyword% " +
-            "OR e.department LIKE %:keyword%")
+    @Query(value="SELECT e FROM User e WHERE  (e.firstName) LIKE %:keyword% " +
+            "OR (e.lastName) LIKE %:keyword% OR (e.username) LIKE %:keyword% " +
+            "OR LOWER(e.department) LIKE %:keyword%")
     Page<User> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
+    /*@Query(value="SELECT e FROM User e WHERE LOWER (e.firstName) LIKE %:keyword% " +
+            "OR LOWER(e.lastName) LIKE %:keyword% OR LOWER(e.username) LIKE %:keyword% " +
+            "OR LOWER(e.department) LIKE %:keyword% OR UPPER(e.firstName) LIKE %:keyword% " +
+            "OR UPPER(e.lastName) LIKE %:keyword% OR UPPER(e.username) LIKE %:keyword% " +
+            "OR UPPER(e.department) LIKE %:keyword%  (e.firstName) LIKE %:keyword% " +
+            "OR e.lastName LIKE %:keyword% OR e.username LIKE %:keyword% " +
+            "OR (e.department) LIKE %:keyword%")*/
 
 }
 
